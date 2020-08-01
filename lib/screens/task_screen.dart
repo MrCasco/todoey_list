9aae4b0bf0bd893d3938e2c14d36fc1bde0df48a
MrCasco <ernesto.casco.velazquez@hotmail.com>
2020-08-01 14:20:13 -0500

    Styling up the list icon and 'todoey' text

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -15,15 +15,24 @@ class TasksScreen extends StatelessWidget {
         child: Column(
           children: <Widget>[
             CircleAvatar(
-              child: Icon (
+              child: Icon(
                 Icons.list,
                 size: 30.0,
+                color: Colors.lightBlueAccent,
               ),
               backgroundColor: Colors.white,
               radius: 30.0,
             ),
+            SizedBox(
+              height: 10.0,
+            ),
             Text(
               'Todoey',
+              style: TextStyle(
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
             ),
           ],
         ),
